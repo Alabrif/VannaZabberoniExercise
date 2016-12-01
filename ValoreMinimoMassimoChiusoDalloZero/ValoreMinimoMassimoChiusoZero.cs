ce79e4562543baf43ea25592403a78d5375da126
SEBASTIANO.GAUDEANO <sebastiano.gaudeano@live.com>
2016-12-01 16:18:28 +0100

    Il programma funziona correttamente

--- ValoreMinimoMassimoChiusoDalloZero/ValoreMinimoMassimoChiusoZero.cs
+++ ValoreMinimoMassimoChiusoDalloZero/ValoreMinimoMassimoChiusoZero.cs
@@ -20,28 +20,43 @@ namespace ValoreMinimoMassimoChiusoDalloZero
             int somma = 0;
 
             int numeroScelto  = 1;
+
             while (numeroScelto != 0)
             {
                 //TODO: DETERMINARE LA SOMMA LA MEDIA VALORE MINIMO E MASSIMO  
-                WriteLine($"Inserire il{indiceMedia + 1}");
-
-                numeroScelto = Convert.ToInt32(Console.ReadLine());
-                if (numeroScelto > numeroMassimo)
+                WriteLine($"Inserire il {indiceMedia + 1}° numero:");
+                int result = 1;
+                string numeroStringa = ReadLine();
+                if (int.TryParse(numeroStringa, out result))
                 {
-                    numeroScelto = numeroMassimo;
-                }
-                else if (numeroScelto < numeroMinimio)
-                {
-                    numeroMinimio = numeroScelto;
-                }
+                    numeroScelto = result;
 
-                somma += numeroScelto;
-                indiceMedia++;
+
+                    if (numeroScelto > numeroMassimo)
+                    {
+                        numeroMassimo = numeroScelto;
+                    }
+                    else if (numeroScelto < numeroMinimio)
+                    {
+                        numeroMinimio = numeroScelto;
+                    }
+
+                    somma += numeroScelto;
+                    indiceMedia++;
+                }
+                else
+                {
+                    WriteLine("Hai sbagliato!!!");
+                }
             }
 
             //TODO: STAMPARE LE INFORMAZIONI A VIDEO:
 
-            WriteLine($"Il numero massimo inserito è: {numeroMassimo}. \nIl numero minimo inserito è: {numeroMinimio}.\nLa media dei numeri inserita è: {somma / indiceMedia}. \nLa somma dei numeri inseriti è: {somma}");
+            WriteLine($"Il numero massimo inserito è: {numeroMassimo}.\n"+
+                $"Il numero minimo inserito è: {numeroMinimio}.\n"+
+                $"La media dei numeri inserita è: {somma / indiceMedia}.\n"+
+                $"La somma dei numeri inseriti è: {somma}");
+            ReadLine();
         }
     }
 }
