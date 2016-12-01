44a7694c1058d5ae95bc4dc7dceefab789e0dc22
SEBASTIANO.GAUDEANO <sebastiano.gaudeano@live.com>
2016-12-01 16:26:49 +0100

    Stampare un quadrato di asterischi di lato n.

---
+++ QuadratoAsterischi/QuadratoAsterischi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using System.Threading.Tasks;
+
+namespace QuadratoAsterischi
+{
+    class QuadratoAsterischi
+    {
+        static void Main(string[] args)
+        {
+
+            //TODO: INSERIRE IL LATO DEL QUADRATO
+            WriteLine("Inserire il lato del quadrato: ");
+
+            int lato = Convert.ToInt32(Console.ReadLine());
+
+            //TODO: STAMPRE IL QUADRATO
+            for (int i = 0; i < lato; i++)
+            {
+                for (int j = 0; j < lato; j++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine("");
+            }
+
+            Read();
+        }
+    }
+}
