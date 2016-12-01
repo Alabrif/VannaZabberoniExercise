2ee919e7c9aee094d41272760ba21f177850b3f7
SEBASTIANO.GAUDEANO <sebastiano.gaudeano@live.com>
2016-12-01 15:24:28 +0100

    Aggiunto progetto triangolo di floyd

---
+++ TriangoloDiFloyd/TriangoloDiFloyd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangoloDiFloyd
+{
+    class TriangoloDiFloyd
+    {
+        static void Main(string[] args)
+        {
+            int k = 1;
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Write($"\t{k}");
+                    k++;
+                }
+                WriteLine();
+            }
+
+            Read();
+        }
+    }
+}
