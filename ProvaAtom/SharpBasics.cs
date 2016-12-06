89f66a2f163c81efba6d5d510930ec015f6304dc
Sebastiano <sebastiano.gaudeano@live.com>
2016-12-06 13:03:19 +0100

    Somma
    Aggiunto un progetto con la somma per il C#

---
+++ ProvaAtom/SharpBasics.cs
@@ -0,0 +1,14 @@
+using System;
+
+/// <summary>
+///   The main entry point for the application
+/// </summary>
+public static void Main(string[] args)
+{
+    int a = 12;
+
+    int b = 12;
+
+    Console.WriteLine($"{a+b}");
+
+}
