a982b4a06a622740d5b0dc2f3bbff06d4f39cc90
Sebastiano <sebastiano.gaudeano@live.com>
2017-01-26 15:31:10 +0100

    Creato il metodo per lo Swap dei valori, testato e funzionante, i valori vengono scambiati nel metodo tramite ref

--- StringraDiAsterischiDaIntero/StringaDiAsterischiDaIntero.cs
+++ StringraDiAsterischiDaIntero/StringaDiAsterischiDaIntero.cs
@@ -10,6 +10,7 @@ namespace StringraDiAsterischiDaIntero
     {
         static void Main ( string[] args )
         {
+
         }
     }
 }
---
+++ SwapValoriRef/SwapValoriRef.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapValoriRef
+{
+    class SwapValoriRef
+    {
+        static void Main ( string[] args )
+        {
+
+            int primoNumero = 21;
+            int secondoNumero = 33;
+
+            Console.WriteLine("Prima dello swap: X: {0}, Y; {1}", primoNumero, secondoNumero);
+
+            SwapValori( ref primoNumero , ref secondoNumero );
+
+            Console.WriteLine( "Dopo lo swap: X: {0}, Y; {1}" , primoNumero , secondoNumero );
+
+        }
+
+
+        /// <summary>
+        /// Metodo per cambiare il valore di due variabili
+        /// </summary>
+        /// <param name="primoNumero"></param>
+        /// <param name="secondoNumero"></param>
+        private static void SwapValori ( ref int primoNumero , ref int secondoNumero )
+        {
+            int temp = 0;
+            temp = primoNumero;
+            primoNumero = secondoNumero;
+            secondoNumero = temp;
+        }
+    }
+}
