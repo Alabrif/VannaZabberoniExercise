ef54eb83c1104798c0ea86477f16abb062fa463d
Sebastiano <sebastiano.gaudeano@live.com>
2017-01-26 16:15:55 +0100

    Creato progetto che crea stringa di asterischi

--- StringraDiAsterischiDaIntero/StringaDiAsterischiDaIntero.cs
+++ StringraDiAsterischiDaIntero/StringaDiAsterischiDaIntero.cs
@@ -10,7 +10,32 @@ namespace StringraDiAsterischiDaIntero
     {
         static void Main ( string[] args )
         {
+            int scelta = 0;
+            string stringaAsterischi = Console.ReadLine();
+            if ( int.TryParse(stringaAsterischi, out scelta) )
+            {
+                Console.WriteLine("Molto bene! \n");
+            }
+            else
+            {
+                Console.WriteLine("Molto male!!\n");
+                Main( args );
+            }
 
+            stringaAsterischi = getAsterischiOnString(scelta);
+
+            Console.WriteLine("La stringa fatta di asterischi: " + stringaAsterischi);
+        }
+
+        private static string getAsterischiOnString ( int scelta )
+        {
+            string stringaAsterischi = "";
+
+            for ( int i = 0 ; i < scelta ; i++ )
+            {
+                stringaAsterischi += "*";
+            }
+            return stringaAsterischi;
         }
     }
 }
