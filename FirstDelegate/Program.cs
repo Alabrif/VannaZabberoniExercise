194840bd917cf7441ced96d96fbc7360b2b2aded
Sebastiano <sebastiano.gaudeano@live.com>
2017-03-12 14:37:41 +0100

    Aggiunto progetto con Delegate, cerchiamo di capire come funzionano

---
+++ FirstDelegate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDelegate
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+        }
+    }
+}
